17463b0242147a376b569d76b93ddb652e224eef
Moonshallow5 <ssandeepssingh55@gmail.com>
2024-10-18 14:07:59 +0800

    data from the json file split up

---
+++ app/src/main/java/com/example/android_map/Recipe.kt
@@ -0,0 +1,10 @@
+package com.example.android_map
+
+data class Recipe(
+    val title: String,
+    val directions: String,
+    val ingredients: String,
+    val calories: String,
+    val category: String // 'V' for Vegetarian, 'P' for Protein, 'D' for Dessert
+
+)
